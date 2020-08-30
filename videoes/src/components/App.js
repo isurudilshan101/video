a6dc0331eae7b00708e6f1283515591ddb05f713
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-30 13:00:50 +0530

    comment 3

--- videoes/src/components/App.js
+++ videoes/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component{
 
     render(){
-        return <div>App</div>;
+        return (
+            <div></div>,
+
+            <div><SearchBar/></div>
+            
+            );
     }
 }
 
