e17098c348b3ba0bcea1bc1659e1336e83e32414
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-31 13:55:11 +0530

    comment 14

--- videoes/src/apis/youtube.js
+++ videoes/src/apis/youtube.js
@@ -5,8 +5,8 @@ const KEY='AIzaSyBi5Ba8W8gQsJOqKujvSU2eRDGAkxO13m8';
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3',
     params:{
-        part:'snipset',
-        maxResult:5,
+        part:'snippet',
+        maxResult: 5,
         key:KEY
     }
 });
--- videoes/src/components/App.js
+++ videoes/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component{
 
     onTermSubmit=(term)=>{
-        console.log(term);
+       youtube.get('/search',{
+           params:{
+               q:term
+           }
+       }
+        
+       );
     };
 
     render(){
