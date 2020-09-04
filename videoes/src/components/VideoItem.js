4d758fdb6da85dd0182269139d4d02807ea399e5
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-04 18:05:50 +0530

    comment 25

--- videoes/src/components/VideoItem.js
+++ videoes/src/components/VideoItem.js
@@ -3,9 +3,14 @@ import React from 'react';
 const VideoItem =({video})=>{
 return( 
 <div className="item">
-    <img src={video.snippet.thumbnails.medium.url}/>
-    {video.snippet.title}
-    </div>);
+    <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+   <div className="content">
+  <div className="header" >
+  {video.snippet.title}
+  </div>
+   </div>
+    </div>
+    );
 };
 
 export default VideoItem;
