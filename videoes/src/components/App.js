b2bb76d6f052703da66eae3a1b6d35d85c7aa429
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-05 13:53:46 +0530

    comment 38

--- videoes/src/components/App.js
+++ videoes/src/components/App.js
@@ -17,8 +17,9 @@ class App extends React.Component{
        );
 
       
-       this.setState({videos: response.data.items,
-                    selectVideo:response.data.items[0]
+       this.setState({
+                    videos: response.data.items,
+                    selectedVideo:response.data.items[0]
                 } );
     };
     onVideoSelect=video=>{
