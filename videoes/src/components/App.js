d51c249a0afa9e66cd1bac3a0adb55958eca9ebf
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-09-05 10:28:12 +0530

    comment 36

--- videoes/src/components/App.js
+++ videoes/src/components/App.js
@@ -30,7 +30,7 @@ class App extends React.Component{
             <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}/>
             <div className="ui grid">
-                <div ui row> 
+                <div className="ui row"> 
 
                 <div className="eleven wide column">
                      <VideoDetail video={this.state.selectedVideo}/>
